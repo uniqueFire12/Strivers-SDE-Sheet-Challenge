093d6a71f7676f17a0cb7ee24cc1ee3898c99af0
Ananya Ganguly <107770579+uniqueFire12@users.noreply.github.com>
2023-06-11 22:21:12 +0530

    Create Stock buy and sell.cpp

---
+++ Arrays Part - I/Stock buy and sell.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int maxProfit = INT_MIN, minPrice = INT_MAX;
+
+    for(int i=0; i<prices.size(); i++) {
+        minPrice = min(minPrice, prices[i]);
+        maxProfit = max(maxProfit, prices[i]-minPrice);
+    }
+
+    return maxProfit;
+}
